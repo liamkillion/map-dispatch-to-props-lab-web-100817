9865e7a835be2f80bd716a8a61c3f611f8d45aac
Liam Killion <liam.killion@gmail.com>
2018-01-10 18:33:02 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import manageRestaurants from './reducers/manageRestaurants';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import manageRestaurants from "./reducers/manageRestaurants";
 
 // we wrap store in a function for testing purposes
 export const configureStore = () => {
-  return createStore(manageRestaurants, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-}
+  return createStore(
+    manageRestaurants,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+};
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App store={store}/>
+    <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
